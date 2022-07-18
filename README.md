31a16da2a3269c5899309b0790f0f9ce86790fb6
samlarson94 <samlarson94@gmail.com>
2022-07-18 11:20:48 -0500

    Initial project schema completed on Sanity. Back-end up and running.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## Ecommerce App
 
+## Description
+- Utilizes Sanity.io for database configuration and management.  Saw this as a solution to save time and energy when setting up and maintaining the backend server, database, and schemas.  
+
 ## Helpful Sanity Commands
 - "sanity docs" = explore sanity studio in browser
 - "sanity manage" = opens up sanity manager in browser
 - "sanity start" = spin up content studio on localhost
+
--- sanity_ecommerce-project/schemas/schema.js
+++ sanity_ecommerce-project/schemas/schema.js
@@ -9,6 +9,7 @@ import category from './category'
 import product from './product'
 import vendor from './vendor'
 import productVariant from './productVariant'
+import banner from './banner'
 
 import localeString from './locale/String'
 import localeText from './locale/Text'
@@ -24,8 +25,7 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     product,
-    vendor,
-    category,
+    banner,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
