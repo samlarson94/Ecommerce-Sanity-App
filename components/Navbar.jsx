b9ed61b6be0bfe85271c85ba7a6971952d977516
samlarson94 <samlarson94@gmail.com>
2022-07-19 15:55:43 -0500

    Adding content to NavBar.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-
 import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 import { urlFor } from '../lib/client'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Speaker City</Link>
+      </p>
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-itm-qty">1</span>
+      </button>
+
+    </div>
   )
 }
 
