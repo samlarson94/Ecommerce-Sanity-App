62aabb47dae0912646932de51acdfcf89701a8d7
samlarson94 <samlarson94@gmail.com>
2022-07-19 15:38:45 -0500

    Setting up Layout.jsx with Head, header, main, and footer.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+// Note: Head = metadata much like in HTML
+import Head from 'next/head'; 
+
+import Navbar from './Navbar';
+import Footer from './Footer';
+
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Speaker City</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+
+      <main className="main-container">
+        EMPTY
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+    
   )
 }
 
