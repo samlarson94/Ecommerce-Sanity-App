3796571819bfd3f66589b43542611bd9e3309f17
samlarson94 <samlarson94@gmail.com>
2022-07-21 11:55:18 -0500

    Adding callback function to Add-to-Cart onClick listener to properly handle product name and qty added.

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -57,7 +57,7 @@ const ProductDetails = ({ product, products }) => {
                     </p>
                 </div>
                 <div className="buttons">
-                    <button type="button" className="add-to-cart" onClick={onAdd}>Add to Cart</button>
+                    <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
                     <button type="button" className="buy-now" onClick="">Buy Now</button>
                 </div>
             </div>
