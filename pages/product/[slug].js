31c8d87a094deb2d99f3e8c6d22d63a62a67485a
samlarson94 <samlarson94@gmail.com>
2022-07-21 10:05:40 -0500

    Adding getStaticProps function from Next.js. Typo in query constant fixed. Product pages now rendering image.

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -19,11 +19,34 @@ const ProductDetails = ({ product, products }) => {
   )
 }
 
+// Add getStaticPaths function to get information for all products but only return the current slug 
+export const getStaticPaths = async () => {
+    // Revise query to filter for slug
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+    // New products constant
+    const products = await client.fetch(query);
+    // Add paths constant to hold product object
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }));
+    // Return paths and a fallback value
+    return {
+        paths, 
+        fallback: 'blocking'
+    }
+}
+
 //ReFactoring getServerSideProps to get static props and render user's requests
     //Use Next.js getStaticProps
 export const getStaticProps = async ({ params: { slug } }) => {
     // Refactor query to look at all products and grab the first one that matches the selected slug.
-    const query = `*[_type == "product" && slug.current == '${slug}][0]`;
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
     // Add query to grab similar products
     const productsQuery = '*[_type == "product"]'
 
