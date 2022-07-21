a03b6b7efb3ee1f481c99d8006e2f0686507d5cc
samlarson94 <samlarson94@gmail.com>
2022-07-21 11:39:46 -0500

    Quantity context logic complete. Can now update quantity on product pages.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
 import { Layout } from '../components';
 import { StateContext } from '../context/stateContext';
-import { Toaster } from 'react-hot-toast'
+
 
 
 function MyApp({ Component, pageProps }) {
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -10,7 +10,7 @@ const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
 
     const [index, setIndex] = useState(0);
-    const { decQty, incQty, qty } = useStateContext;
+    const { decQty, incQty, qty } = useStateContext();
 
     return (
     <div>
@@ -51,16 +51,10 @@ const ProductDetails = ({ product, products }) => {
                 <div className="quantity">
                     <h3>Quantity: </h3>
                     <p className='quantity-desc'>
-                        <span className='minus' onClick="">
-                            <AiOutlineMinus />
-                        </span>
-                        <span className='num'>
-                            0
-                        </span>
-                        <span className='plus' onClick="">
-                            <AiOutlinePlus />
-                        </span>
-                        </p>
+                        <span className='minus' onClick={decQty}> <AiOutlineMinus /> </span>
+                        <span className='num' onClick="">{qty}</span>
+                        <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
+                    </p>
                 </div>
                 <div className="buttons">
                     <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
