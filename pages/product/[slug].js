b024bc41128e3932485af5ff538918f3dff52971
samlarson94 <samlarson94@gmail.com>
2022-07-21 11:09:16 -0500

    exporting useStateContext from stateContext.js, adding and destructuring qty functions in [slug].js.

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/stateContext';
 
 
 const ProductDetails = ({ product, products }) => {
@@ -9,6 +10,7 @@ const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
 
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty } = useStateContext;
 
     return (
     <div>
