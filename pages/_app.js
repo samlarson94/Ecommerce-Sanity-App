eeef4fc6de20dc8fecf9e1d0e503f4276d6bd566
samlarson94 <samlarson94@gmail.com>
2022-07-21 11:00:17 -0500

    Adding Toaster component to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import '../styles/globals.css'
 import { Layout } from '../components';
-import { StateContext } from '../context/stateContext'
+import { StateContext } from '../context/stateContext';
+import { Toaster } from 'react-hot-toast'
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext> 
