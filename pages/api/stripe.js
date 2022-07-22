8746ea31871a8db332dc8507c348c0eb81324908
samlarson94 <samlarson94@gmail.com>
2022-07-22 12:53:41 -0500

    Adding additional params, adding shipping options.

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -3,8 +3,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+            { shipping_rate: 'price_1LOQ0UHXrL1AAVi3pG0g4Jco'}
+            { shipping_rate: 'price_1LOQ1THXrL1AAVi3vbTnnYSe'}
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -15,7 +22,9 @@ export default async function handler(req, res) {
         mode: 'payment',
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
+      }
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
       res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
