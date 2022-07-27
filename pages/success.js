9474e74e1f0c5d7b60642861f4447e750960ddd9
samlarson94 <samlarson94@gmail.com>
2022-07-27 16:25:09 -0500

    Renaming to success to simplify, adding page structure and CSS.

---
+++ pages/success.js
@@ -0,0 +1,23 @@
+import React, { useState, useEfffect } from 'react';
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+
+import { useStateContext } from '../context/stateContext';
+
+const Success = () => {
+    const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  return (
+    <div className="success-wrapper">
+        <div className='success'>
+            <p className='icon'>
+                <BsBagCheckFill />
+            </p>
+            <h2>Thank you for your order!</h2>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Success;
